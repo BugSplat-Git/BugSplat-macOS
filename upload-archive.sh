0e369989f1608c31aa92efd32baf986ccc59a3ab
Billy Flaherty <bflaherty4@gmail.com>
2016-04-20 19:33:08 -0400

    Updates xcarchive bash upload script to authenticate the developer and include the session in the upload request.

--- upload-archive.sh
+++ upload-archive.sh
@@ -3,8 +3,12 @@
 # (Above line comes out when placing in Xcode scheme)
 #
 
-DATABASE="bugsplattester"
-UPLOAD_URL="http://${DATABASE}.bugsplatsoftware.com/post/mac/symbols/"
+DATABASE=""
+BUGSPLAT_USER=""
+BUGSPLAT_PASS=""
+#BUGSPLAT_DOMAIN="${DATABASE}.bugsplatsoftware.com"
+BUGSPLAT_DOMAIN="oban.bugsplatsoftware.com"
+UPLOAD_URL="https://${BUGSPLAT_DOMAIN}/post/plCrashReporter/symbol/"
 
 LOG="/tmp/bugsplat-upload.log"
 DATE=$( /bin/date +"%Y-%m-%d" )
@@ -24,6 +28,13 @@ cd "${ARCHIVE_DIR}/${ARCHIVE}"
 /usr/bin/zip -r "/tmp/${PRODUCT_NAME}.xcarchive.zip" *
 cd -
 
+echo "Signing into bugsplat and storing session cookie for use in upload" >> $LOG 2>&1
+
+COOKIEPATH="/tmp/bugsplat-cookie.txt"
+rm "${COOKIEPATH}"
+curl -b "${COOKIEPATH}" -c "${COOKIEPATH}" "https://${BUGSPLAT_DOMAIN}/login"
+curl -b "${COOKIEPATH}" -c "${COOKIEPATH}" --data "currusername=${BUGSPLAT_USER}&currpasswd=${BUGSPLAT_PASS}" "https://${BUGSPLAT_DOMAIN}/browse/login.php"
+
 echo "Uploading /tmp/${PRODUCT_NAME}.xcarchive.zip to ${UPLOAD_URL}" >> $LOG 2>&1
 
-curl -i -F filedata=@"/tmp/${PRODUCT_NAME}.xcarchive.zip" -F appName="${PRODUCT_NAME}" -F appVersion="${APP_VERSION}" $UPLOAD_URL >> $LOG 2>&1
+curl -i -b "${COOKIEPATH}" -c "${COOKIEPATH}" -F filedata=@"/tmp/${PRODUCT_NAME}.xcarchive.zip" -F appName="${PRODUCT_NAME}" -F appVer="${APP_VERSION}" -F database="${DATABASE}" $UPLOAD_URL >> $LOG 2>&1
