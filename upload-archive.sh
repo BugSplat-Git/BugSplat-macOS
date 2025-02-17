be0e5d574fb9319e6afb6b644024ce1ed3597822
Dave Plunkett <dave@bugsplat.com>
2025-02-17 21:56:39 +0000

    Changed authentication to use /api/authenticatev3.php (#17)

--- upload-archive.sh
+++ upload-archive.sh
@@ -65,10 +65,10 @@ echo "UUID found: ${UUID_CMD_OUT}" > $LOG 2>&1
 echo "Signing into bugsplat and storing session cookie for use in upload" >> $LOG 2>&1
 
 COOKIEPATH="/tmp/bugsplat-cookie.txt"
-LOGIN_URL="${BUGSPLAT_SERVER_URL}/browse/login.php"
+LOGIN_URL="${BUGSPLAT_SERVER_URL}/api/authenticatev3.php"
 echo "Login URL: ${LOGIN_URL}"
 rm "${COOKIEPATH}"
-curl -b "${COOKIEPATH}" -c "${COOKIEPATH}" --data-urlencode "currusername=${BUGSPLAT_USER}" --data-urlencode "currpasswd=${BUGSPLAT_PASS}" "${LOGIN_URL}"
+curl -b "${COOKIEPATH}" -c "${COOKIEPATH}" --data-urlencode "email=${BUGSPLAT_USER}" --data-urlencode "password=${BUGSPLAT_PASS}" "${LOGIN_URL}"
 
 echo "Uploading /tmp/${PRODUCT_NAME}.xcarchive.zip to ${UPLOAD_URL}" >> $LOG 2>&1
 
